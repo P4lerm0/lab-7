5a316219fe2723c86c5b58d8ad25813e935c461f
P4lerm0 <sasaermakov77@gmail.com>
2021-12-20 23:02:41 +0300

    Добавьте файлы проекта.

---
+++ lab 7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lab7.part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] D = { 'а', 'б', 'в', 'г', 'д', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я' };
+            char[] a = { 'а', 'F', 'в', 'F', ',', 'А', 'з', 'и', 'й', 'к' };
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < D.Length; j++)
+                    if (Char.IsLower(a[i]) && a[i] == D[j])
+                        Console.Write(a[i]);
+            }
+        }
+    }
+}
---
+++ part 2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace lab7.part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = ",?.;:'";
+            Console.WriteLine("Количество знаков препинания в первой строке = " + str.Count(char.IsPunctuation));
+        }
+    }
+}
